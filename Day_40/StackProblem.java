08dd4642994d299755d3ae4fbb579c40341b9247
Navee267 <navinraja0681@gmail.com>
2025-08-24 21:28:30 +0530

    Day 40 of Learning DSA (Queue Problem Solving)

---
+++ Day_40/StackProblem.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+public class StackProblem {
+    public boolean checkValidString(String s) {
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+        
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                open.push(i);
+            }
+            else if(ch == '*'){
+                star.push(i);
+            }
+            else{
+                if(!open.isEmpty()){
+                    open.pop();
+                }
+                else if(!star.isEmpty()){
+                    star.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        while(!open.isEmpty() && !star.isEmpty()){
+            if(open.peek() > star.peek()){
+                return false;
+            }
+            open.pop();
+            star.pop();
+        }
+        return open.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        StackProblem obj =new StackProblem();
+        String str = "(*))";
+        boolean res = obj.checkValidString(str);
+        System.out.println(res);
+    }
+}
